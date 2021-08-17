1dcb831ed1101955a52bde2633e438b769d4e0b8
IUDljKkB <andrii.linkoln@gmail.com>
2021-08-17 11:29:16 +0200

    implement class to count failed and passed tests

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-
 class TestCase:
 
     def __init__(self, name):
         self.name = name
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
-        method()
+        try:
+            method()
+        except Exception:
+            result.testFailed()
         self.tearDown()
+        return result
 
     def setUp(self):
         pass
@@ -34,14 +39,52 @@ class WasRun(TestCase):
     def tearDown(self):
         self.log += "tearDown "
 
+    def testBrokenMethod(self):
+        raise Exception
+
 
 class TestCaseTest(TestCase):
     # Arrange -> Act -> Assert
 
     def testTemplateMethod(self):
-        self.test = WasRun("testMethod")
-        self.test.run()
-        assert "setUp testMethod tearDown " == self.test.log
+        test = WasRun("testMethod")
+        test.run()
+        assert "setUp testMethod tearDown " == test.log
+
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert "1 run, 0 failed" == result.summary()
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert "1 run, 1 failed" == result.summary()
+
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert "1 run, 1 failed" == result.summary()
+
+
+class TestResult:
+
+    def __init__(self):
+        self.runCount = 0
+        self.errorCount = 0
+
+    def testStarted(self):
+        self.runCount += 1
+
+    def testFailed(self):
+        self.errorCount += 1
+
+    def summary(self):
+        return f"{self.runCount} run, {self.errorCount} failed"
 
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
+#
