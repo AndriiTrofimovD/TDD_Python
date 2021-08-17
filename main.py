bd301de7332781b969bf5bcffd427a1fb7c98649
IUDljKkB <andrii.linkoln@gmail.com>
2021-08-17 10:28:35 +0200

    implement 'setUp' method and make it run first

--- main.py
+++ main.py
@@ -5,27 +5,36 @@ class TestCase:
         self.name = name
 
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
+
+    def setUp(self):
+        pass
 
 
 class WasRun(TestCase):
 
     def __init__(self, name):
         TestCase.__init__(self, name)
-        self.wasRun = None
 
     def testMethod(self):
         self.wasRun = 1
 
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
+
 
 class TestCaseTest:
+    # Arrange -> Act -> Assert
+    def setUp(self):
+        self.test = WasRun("testMethod")
 
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
+        self.test.run()
+        assert self.test.wasRun
 
-
-
+    def testSetUp(self):
+        self.test.run()
+        assert self.test.wasSetUp
